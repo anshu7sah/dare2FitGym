39cb5520d52801eae974a9de58bb3636405e2555
Anshu Sah <anshu7sah@gmail.com>
2024-10-16 01:07:10 +0545

    added autoplay and hover effect in slider

--- src/components/swiper/Card.tsx
+++ src/components/swiper/Card.tsx
@@ -1,26 +1,40 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 
 interface CardProps {
-  svg: ReactNode;
+  svg: (props: any) => ReactNode;
   background: string;
   title: string;
   description: string;
 }
 
-const Card: React.FC<CardProps> = ({ svg, background, title, description }) => {
+const Card: React.FC<CardProps> = ({
+  svg: SvgIcon,
+  background,
+  title,
+  description,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const svgFill = isHovered ? "#ffffff" : "#007bff";
+  const hoverBackgroundColor = isHovered ? "#007bff" : background;
   return (
-    <div className="flex justify-center items-center">
+    <div
+      className="flex justify-center items-center rounded-xl"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div
-        className="w-[17rem] p-6 rounded-xl text-white transition-all duration-300 ease-in-out transform hover:scale-105"
+        className="w-[17rem] px-7 py-10 rounded-xl text-white transition-all duration-300 ease-in-out transform hover:scale-105"
         style={{
-          backgroundColor: background || "#1b1b1b", // Slightly lighter than black background
+          backgroundColor: hoverBackgroundColor,
           boxShadow: "0px 6px 15px rgba(0, 0, 0, 0.25)", // Soft shadow for better depth
           cursor: "pointer",
         }}
       >
         {/* SVG Section */}
-        <div className="mb-4">{svg}</div>
+        <div className="mb-4">
+          <SvgIcon fill={svgFill} />
+        </div>
         {/* Title */}
         <h3 className="text-lg font-semibold mb-2 text-left">{title}</h3>
         {/* Description */}
--- src/components/swiper/SwiperComp.tsx
+++ src/components/swiper/SwiperComp.tsx
@@ -6,7 +6,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import { Navigation } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 import CardioStrengthIcon from "../svg/CardioStrengthIcon";
 import MuscleGain from "../svg/MuscleGain";
 import FatLost from "../svg/FatLost";
@@ -15,50 +15,85 @@ import Card from "./Card";
 
 const cardData = [
   {
-    svg: <CardioStrengthIcon />,
-    background: "#007bff", // Blue background for the selected card
+    svg: (props: any) => <CardioStrengthIcon {...props} />,
+    background: "#1b1b1b",
     title: "Cardio Strength",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
   },
   {
-    svg: <FatLost />,
-    background: "#1b1b1b", // Dark background for other cards
+    svg: (props: any) => <FatLost {...props} />,
+    background: "#1b1b1b",
     title: "Fat Lose",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
   },
   {
-    svg: <MuscleGain />,
-    background: "#1b1b1b", // Dark background for other cards
+    svg: (props: any) => <FatLost {...props} />,
+    background: "#1b1b1b",
+    title: "Fat Lose",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
+  },
+  {
+    svg: (props: any) => <FatLost {...props} />,
+    background: "#1b1b1b",
+    title: "Fat Lose",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
+  },
+  {
+    svg: (props: any) => <FatLost {...props} />,
+    background: "#1b1b1b",
+    title: "Fat Lose",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
+  },
+  {
+    svg: (props: any) => <FatLost {...props} />,
+    background: "#1b1b1b",
+    title: "Fat Lose",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
+  },
+  {
+    svg: (props: any) => <FatLost {...props} />,
+    background: "#1b1b1b",
+    title: "Fat Lose",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
+  },
+  {
+    svg: (props: any) => <MuscleGain {...props} />,
+    background: "#1b1b1b",
     title: "Muscle Gain",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
   },
   {
-    svg: <Nutritions />,
-    background: "#1b1b1b", // Dark background for other cards
+    svg: (props: any) => <Nutritions {...props} />,
+    background: "#1b1b1b",
     title: "Nutrition",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
   },
   {
-    svg: <Nutritions />,
-    background: "#1b1b1b", // Dark background for other cards
+    svg: (props: any) => <Nutritions {...props} />,
+    background: "#1b1b1b",
     title: "Nutrition",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
   },
   {
-    svg: <Nutritions />,
-    background: "#1b1b1b", // Dark background for other cards
+    svg: (props: any) => <Nutritions {...props} />,
+    background: "#1b1b1b",
     title: "Nutrition",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
   },
   {
-    svg: <Nutritions />,
-    background: "#1b1b1b", // Dark background for other cards
+    svg: (props: any) => <Nutritions {...props} />,
+    background: "#1b1b1b",
     title: "Nutrition",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, nunc a pretium viverra.",
@@ -68,24 +103,28 @@ const cardData = [
 const SwiperComp = () => {
   return (
     <div className="py-10 bg-black relative overflow-hidden">
-      {/* Background text */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[10rem] text-white opacity-10 font-bold tracking-wide">
+      <div
+        className="absolute top-0 left-1/2 transform -translate-x-[100%] -translate-y-[10%] text-white opacity-10 font-bold tracking-wide z-0 pointer-events-none
+        text-[5rem] md:text-[7rem] lg:text-[10rem] xl:text-[12rem]"
+      >
         Program
       </div>
 
-      {/* Main heading */}
-      <h2 className="relative text-center text-3xl font-bold text-white z-10 mb-10">
+      <h2
+        className="relative text-center font-bold text-white  mb-5
+        text-4xl md:text-5xl lg:text-7xl xl:text-8xl"
+      >
         Explore Our Program
       </h2>
 
       <Swiper
-        modules={[Navigation]}
+        modules={[Autoplay]}
         spaceBetween={30}
         slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
+        // navigation={true}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        // pagination={{ clickable: true }}
         loop={true}
-        className="pt-[20px]"
         breakpoints={{
           640: {
             slidesPerView: 2,
