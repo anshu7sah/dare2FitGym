1cf7be85c77fcfcf1ef6a9a139f0ea4b682d8b28
Anshu Sah <anshu7sah@gmail.com>
2024-10-20 18:53:28 +0545

    completed pricing section

--- src/components/package/Package.tsx
+++ src/components/package/Package.tsx
@@ -3,12 +3,135 @@ import React, { useState } from "react";
 import PricingCard from "./PricingCard";
 import { Button } from "../ui/button";
 
+const monthlyData = [
+  {
+    name: "Basic Package",
+    price: "1000",
+    features: [
+      "Unlimited Gym Access",
+      "2x Fitness Consultant",
+      "Nutrition Tracking",
+      "1x Free Suplement",
+      "3 Days per week",
+      "Personal Trainer",
+    ],
+    bestOffer: false,
+  },
+
+  {
+    name: "Pro Package",
+    price: "3000",
+    features: [
+      "Unlimited Gym Access",
+      "7x Fitness Consultant",
+      "Nutrition Tracking",
+      "5x Free Suplement",
+      "Gym Card",
+      "Personal Trainer",
+    ],
+    bestOffer: false,
+  },
+  {
+    name: "Mid Package",
+    price: "2000",
+    features: [
+      "Unlimited Gym Access",
+      "4x Fitness Consultant",
+      "Nutrition Tracking",
+      "3x Free Suplement",
+      "5 Days per week",
+      "Personal Trainer",
+    ],
+    bestOffer: true,
+  },
+  {
+    name: "Athlete Package",
+    price: "5000",
+    features: [
+      "Unlimited Gym Access",
+      "Free Clothes",
+      "All Training Program",
+      "Free Fitness Consultant",
+      "Free Suplement",
+      "Gym Card",
+    ],
+    bestOffer: false,
+  },
+];
+const yearlyData = [
+  {
+    name: "Basic Package",
+    price: "11000",
+    features: [
+      "Unlimited Gym Access",
+      "2x Fitness Consultant",
+      "Nutrition Tracking",
+      "1x Free Suplement",
+      "3 Days per week",
+      "Personal Trainer",
+    ],
+    bestOffer: false,
+  },
+  {
+    name: "Mid Package",
+    price: "20000",
+    features: [
+      "Unlimited Gym Access",
+      "4x Fitness Consultant",
+      "Nutrition Tracking",
+      "3x Free Suplement",
+      "5 Days per week",
+      "Personal Trainer",
+    ],
+    bestOffer: true,
+  },
+  {
+    name: "Pro Package",
+    price: "28000",
+    features: [
+      "Unlimited Gym Access",
+      "7x Fitness Consultant",
+      "Nutrition Tracking",
+      "5x Free Suplement",
+      "Gym Card",
+      "Personal Trainer",
+    ],
+    bestOffer: false,
+  },
+  {
+    name: "Athlete Package",
+    price: "45000",
+    features: [
+      "Unlimited Gym Access",
+      "Free Clothes",
+      "All Training Program",
+      "Free Fitness Consultant",
+      "Free Suplement",
+      "Gym Card",
+    ],
+    bestOffer: false,
+  },
+];
+
 const Package = () => {
   const [billingCycle, setBillingCycle] = useState("monthly");
+  const [packageData, setPackageData] = useState(
+    billingCycle === "monthly" ? monthlyData : yearlyData
+  );
+
+  const bestOfferIndex = packageData.findIndex((item) => item.bestOffer);
+  let reorderedData = [...packageData];
+  if (bestOfferIndex !== 1) {
+    if (bestOfferIndex > -1) {
+      const [bestOfferPackage] = reorderedData.splice(bestOfferIndex, 1);
+      reorderedData.splice(1, 0, bestOfferPackage);
+      setPackageData(() => [...reorderedData]);
+    }
+  }
 
   return (
     <section>
-      <div className="flex flex-col items-center justify-center gap-2 py-12 px-4 sm:px-6 lg:px-8 bg-black text-white">
+      <div className="flex flex-col items-center justify-center gap-2 py-12 px-4 sm:px-6 lg:px-8 bg-black text-white overflow-hidden">
         {/* Title Section */}
         <h3 className="text-blue-700 font-semibold text-lg mb-2">Pricing</h3>
         <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-6">
@@ -23,7 +146,10 @@ const Package = () => {
                 ? "bg-black text-[#ddd]"
                 : "bg-transparent text-[#999]"
             }`}
-            onClick={() => setBillingCycle("monthly")}
+            onClick={() => {
+              setBillingCycle("monthly");
+              setPackageData([...monthlyData]);
+            }}
           >
             Billed Monthly
           </Button>
@@ -33,22 +159,21 @@ const Package = () => {
                 ? "bg-black text-[#ddd]"
                 : "bg-transparent text-[#999]"
             }`}
-            onClick={() => setBillingCycle("yearly")}
+            onClick={() => {
+              setBillingCycle("yearly");
+              setPackageData([...yearlyData]);
+            }}
           >
             Billed Yearly
           </Button>
         </div>
 
-        {/* Add content below based on billingCycle */}
-        <div>
-          {billingCycle === "monthly" ? (
-            <p className="text-center">Show monthly billing packages</p>
-          ) : (
-            <p className="text-center">Show yearly billing packages</p>
-          )}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {packageData.map((p, i) => (
+            <PricingCard data={p} key={i} />
+          ))}
         </div>
       </div>
-      <PricingCard />
     </section>
   );
 };
--- src/components/package/PricingCard.jsx
+++ src/components/package/PricingCard.jsx
@@ -1,43 +1,52 @@
 import { TiTick } from "react-icons/ti";
 import { Button } from "../ui/button";
 
-const features = [
-  "Unlimited Gym Access",
-  "4x Fitness Consultant",
-  "Nutrition Tracking",
-  "3x Free Suplement",
-  "5 Days per week",
-  "Personal Trainer",
-];
-
-const PricingCard = () => {
+const PricingCard = ({ data: { name, price, features, bestOffer } }) => {
   return (
-    <div className="max-w-[19rem] mx-auto bg-blue-500 text-white rounded-2xl overflow-hidden shadow-lg relative p-8 ">
-      <div className="absolute top-0 right-0 h-8 w-[180px] text-center bg-white text-black text-xs font-bold px-2 py-1 transform rotate-45 translate-x-12 translate-y-5 shadow-md shadow-gray-500">
-        <span className="flex items-center justify-center h-full ">
-          Best Offer
-        </span>
-      </div>
+    <div
+      className={`max-w-[19rem] mx-auto ${
+        bestOffer ? "bg-blue-500" : " bg-[#1b1b1b]"
+      }  text-white rounded-2xl overflow-hidden shadow-lg relative p-8`}
+    >
+      {bestOffer && (
+        <div className="absolute top-0 right-0 h-8 w-[180px] text-center bg-white text-black text-xs font-bold px-2 py-1 transform rotate-45 translate-x-12 translate-y-5 shadow-md shadow-gray-500">
+          <span className="flex items-center justify-center h-full ">
+            Best Offer
+          </span>
+        </div>
+      )}
 
       {/* Package Title */}
-      <h3 className="text-lg font-semibold mb-4">Mid Package</h3>
+      <h3
+        className={`text-lg font-semibold mb-4 ${
+          !bestOffer && "text-blue-500"
+        }`}
+      >
+        {name}
+      </h3>
 
       {/* Price */}
-      <div className="text-5xl font-bold mb-4">$55</div>
-      <p className="text-sm">per month, billed annually</p>
+      <div className="text-4xl font-bold mb-4">Rs. {price}</div>
+      <p className={`text-sm ${!bestOffer && "text-[#666]"}`}>
+        per month, billed annually
+      </p>
 
       <ul className="mt-4 space-y-5 mb-10">
         {features.map((feature, index) => (
           <li className="flex items-center gap-4 font-semibold" key={feature}>
             <div className="w-5 h-5 sm:w-6 sm:h-6 bg-white flex items-center justify-center rounded-full">
-              <TiTick size={20} fill="#2196F3" />
+              <TiTick size={20} fill={bestOffer ? "#2196F3" : "#1b1b1b"} />
             </div>
             {feature}
           </li>
         ))}
       </ul>
       <div className="w-full flex items-center justify-center">
-        <Button className="py-7 px-16 bg-black font-semibold">
+        <Button
+          className={`py-7 px-16 ${
+            bestOffer ? "bg-black" : "bg-[#2196F3] hover:bg-blue-700"
+          } font-semibold`}
+        >
           Register Now
         </Button>
       </div>
