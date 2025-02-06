1d4315c91adde34bc3bdf45c6b64cb0a67ecd881
Anshu Sah <anshu7sah@gmail.com>
2025-02-07 00:03:33 +0545

    solved eslint problem in api/routes page

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -5,11 +5,11 @@ import auth from "@/features/auth/server/route";
 
 const app = new Hono().basePath("/api");
 
-const routes = app.route("/auth", auth);
+app.route("/auth", auth);
 
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
 
-export type AppType = typeof routes;
+export type AppType = ReturnType<typeof app.route>;
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,7 +1,14 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { loginSchema, registerSchema } from "../schemas";
 
-const app = new Hono().get("/current", (c) => {
-  return c.json({ anshu: "is handsome" });
-});
+const app = new Hono()
+  .get("/current", (c) => {
+    return c.json({ anshu: "is handsome" });
+  })
+
+  .post("/register", zValidator("json", registerSchema), async (c) => {
+    const { name, email, password } = c.req.valid("json");
+  });
 
 export default app;
